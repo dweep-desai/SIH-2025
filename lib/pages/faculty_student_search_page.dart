e3c5223e74dbffcac9ef95fbf0166809ad2a681e
TathyaBarot2808 <114234807+TathyaBarot2808@users.noreply.github.com>
2025-09-16 15:13:04 +0530

    Update faculty_student_search_page.dart

--- lib/pages/faculty_student_search_page.dart
+++ lib/pages/faculty_student_search_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../widgets/main_drawer.dart';
+import '../widgets/faculty_drawer.dart';
 
 class FacultyStudentSearchPage extends StatefulWidget {
   const FacultyStudentSearchPage({super.key});
