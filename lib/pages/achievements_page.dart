67b03ecfb3b3da3b0526df3d92aa7bc92f9c17b4
Pranshul Soni <67196385+PranshulSoni@users.noreply.github.com>
2025-09-16 16:14:39 +0530

    Update achievements_page.dart

--- lib/pages/achievements_page.dart
+++ lib/pages/achievements_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../widgets/main_drawer.dart';
+import '../widgets/student_drawer.dart';
 import '../widgets/achievement_widgets.dart';
 
 // ---------------- ACHIEVEMENTS PAGE ----------------
